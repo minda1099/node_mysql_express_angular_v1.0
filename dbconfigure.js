8695b412efed0f2b838edaf491a4998d37421608
ndhok <nilam.dhok18@gmail.com>
2014-07-11 10:10:08 +0530

    Updated dbconfigure.js

--- dbconfigure.js
+++ dbconfigure.js
@@ -15,13 +15,28 @@ exports.setupDB = function(conn)
 		});
 	}
 		
-	connection.query('CREATE TABLE IF NOT EXISTS signusers(usrid int(50) primary key not null auto_increment,usrname varchar(50) not null,usremail varchar(100) not null,pswd varchar(50) not null);', function(err) 
-	{
-		if (err) return console.log(err);
-	});
+	connection.query('CREATE TABLE IF NOT EXISTS signusers'+
+	                 '(usrid int(50) primary key not null auto_increment,'+
+					 'usrname varchar(50) not null,'+
+					 'usremail varchar(100) not null,'+
+					 'pswd varchar(50) not null);',
+					 function(err) {
+						if (err) return console.log(err);
+					});
 };
 
 exports.addUserData = function(data, callback)
 {
 	connection.query("INSERT INTO signusers(usrname,usremail,pswd) VALUES (?,?,?)",[data.username,data.useremail,data.password], callback);
 };
+
+exports.getUserData = function(logemail,logpass, callback)
+{
+	connection.query("SELECT usremail, pswd from signusers where usremail=? and pswd=?",[logemail,logpass], function(err,result) {
+		
+		if (err) 
+            callback(err,null);
+        else
+            callback(null,result[0]);
+	});
+}
