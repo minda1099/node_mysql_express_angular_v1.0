26940c37dd66eb13e3afd245143bcc4341a44558
ndhok <nilam.dhok18@gmail.com>
2014-07-11 10:17:39 +0530

    Updated project.js

--- public/lib/project.js
+++ public/lib/project.js
@@ -1,32 +1,31 @@
-	
-var gimme = angular.module('gimme',['mysql']);
+
+
+var gimme = angular.module('gimme',['mysql','logmysql']);
 	
 	gimme.config(['$routeProvider',function($routeProvider) 
 	{
 		$routeProvider.when('/',
 		{
-			templateUrl : 'signup.html',
-			controller : 'mainCtrl'
+			controller: 'mainCtrl',
+			templateUrl: 'signup.html'
+			
 		}).
 		when('/logpg', 
 		{
-			templateUrl : 'login.html',
-			controller : 'loginCtrl'
+			controller: 'loginCtrl',
+			templateUrl: 'login.html'
+		}).
+		when('/usrprofile/',
+		{
+			controller:'userprofile',
+			templateUrl : 'myprofile.html'
 		});
 	}]);
 	
-	function loginCtrl($scope)
-	{ 
-		$scope.loginUser = function()
-		{
-			alert("Login Page Under Development !");
-		}
-	}
-	
-	gimme.controller('mainCtrl', function($scope, $location, SignUpUser)
+	gimme.controller('mainCtrl', function($scope, SignUpUser)
 	{
 		$scope.NAME_REGEXP = /^[a-zA-Z_.-]+[0-9a-zA-Z_]*$/i;
-		$scope.EMAIL_REGEXP = /^[a-z0-9!#$%&'*+/=?^_`{|}~.-]+@[a-z0-9-]+(\.[a-z0-9-]+)*$/i;
+		$scope.EMAIL_REGEXP = /^[A-Za-z0-9]+(.[_A-Za-z0-9-]+)*@[A-Za-z0-9]+(.[A-Za-z0-9]+)*(.[A-Za-z]{2,})$/;
 		$scope.VPSWD = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,30}$/i;
 		
 		$scope.registerUser = function()
@@ -34,6 +33,29 @@ var gimme = angular.module('gimme',['mysql']);
 			SignUpUser.save($scope.user, function(user) 
 			{
 				alert("Sign Up Successful");
+			});
+		}
+	});
+	
+	gimme.controller('userprofile', function($scope) {
+		alert("Loading...");
+	});
+			
+	gimme.controller('loginCtrl', function($scope, $location, LoginUser)	
+	{
+		$scope.loginUser = function()
+		{
+			LoginUser.save($scope.loguser, function(loguser)
+			{
+				alert(loguser.usremail);
+				if(loguser.usremail==undefined || loguser.pswd == undefined)
+				{
+					alert("Invalid Login / Please Sign up with "+$scope.loguser.logemail);
+				}
+				else
+				{
+					$location.path('/usrprofile/');
+				}
 			});
 		}
 	});
